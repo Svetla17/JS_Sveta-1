381f78e9f96506c62201e803052ba243920c5626
Svetla17 <87719525+Svetla17@users.noreply.github.com>
2021-10-21 21:54:36 +0300

    Add files via upload
    axs

---
+++ app.js
@@ -0,0 +1,82 @@
+// const form = document.forms["form"];
+// const button = form.elements["submit"];
+// const input1 = form.elements["text1"];
+// const input2 = form.elements["text2"];
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     console.log(input1.value);
+//     console.log(input2.value);
+//     button.disabled = true;
+// });
+
+// const form1 = document.forms["form1"];
+// const button1 = form.elements["submit"];
+// const input3 = form.elements["text1"];
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     input3.value = " hallo ";
+// });
+
+
+// const form = document.forms["form"];
+// const button1 = form.elements["submit"];
+// const input1 = form.elements["text1"];
+// const input2 = form.elements["checkbox1"];
+// const input3 = form.elements["checkbox2"];
+// const select = form.elements["select"];
+
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     if (input2.checked) {
+//         console.log(input2.value);
+//     }
+//     if (input3.checked) {
+//         console.log(input3.value);
+//     }
+
+//     console.log(input1.value);
+
+// });
+// input1.addEventListener("input", function (event) {
+//     console.log(this.value);
+// })
+// input2.addEventListener("focus", function (event) {
+//     console.log(this.focus);
+// })
+
+// input3.addEventListener("blur", function (event) {
+//     console.log(this.blur);
+// })
+
+
+
+// input1.focus();
+// input1.setSelectionRange(0, 3)
+
+// input1.addEventListener("cut", function (event) {
+//     console.log(this.cut);
+// })
+
+// input1.addEventListener("paste", function (event) {
+//     console.log(this.paste);
+// })
+
+// input1.addEventListener("copy", function (event) {
+//     console.log(this.copy);
+// })
+// input1.addEventListener("copy", function (event) {
+//     console.log(this.copy);
+// })
+
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     console.log(form.checkValidity());
+// });
+// button1.addEventListener("click", function () {
+//     console.log("validation", form.checkValidity());
+// })
+
